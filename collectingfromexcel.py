8fa0d4da240357da1f666f352d25956d03509bbd
cturner6701 <cturner6701@gmail.com>
2026-01-14 21:32:12 -0600

    Update collectingfromexcel.py callback

--- collectingfromexcel.py
+++ collectingfromexcel.py
@@ -1,11 +1,11 @@
-import csv
+import csv delete
 file = open("updatedpage.csv")
 csvreader = csv.reader(file)
 header = next(csvreader)
 header.pop(0)
-print(header)
-rows = []
-for row in csvreader:
+print(header)clone
+rows = []im horny
+for row in csvreader:callback
     rows.append(row)
 print(rows)
-file.close()
+null.
